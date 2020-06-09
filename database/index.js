92e1b87f94670b132a2218e1ef93ac79561d2f83
Alex Benko <benkoalex521@gmail.com>
2020-06-09 09:41:02 -0700

    Finalized connection

--- database/index.js
+++ database/index.js
@@ -4,3 +4,15 @@ const client = new Pool({
   user: 'root',
   database: 'postgres'
 });
+
+client.connect()
+  .then((err)=>{
+    console.log('Connected To Database Successfully');
+  })
+  .catch((err)=>{
+    throw err;
+  });
+
+  module.exports = client;
+
+
