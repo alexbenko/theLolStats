cf586ec8555b6b72403c053000568c230add1e4b
Alex Benko <benkoalex521@gmail.com>
2020-06-09 20:11:20 -0700

    Function now removes spaces and apostrophes from character name strings

--- client/src/components/sr/getChampionName.js
+++ client/src/components/sr/getChampionName.js
@@ -1,10 +1,27 @@
 //converts the championid to a champion name
 import champInfo from './championId.js';
+//const champInfo = require('./championId.js');
 
 let getChampionName = (idPair) =>{
   let champName = champInfo[idPair];
 
-  return champName
+  if(champName.includes("'")){
+    //champName.replace("'", "");
+    let regex = /\'/;
+    let newChampName = champName.replace(regex,"");
+    console.log(newChampName);
+    return newChampName
+  } else if(champName.includes(" ")) {
+    let regex = / /;
+    let newChampName = champName.replace(" ","");
+    console.log(newChampName);
+    return newChampName
+  } else {
+    return champName;
+  }
+
 };
+
+getChampionName('64');
 
 export default getChampionName;
