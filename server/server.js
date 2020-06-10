0a1dda2a03f013c27dd632c530adc278a20bb8c0
Alex Benko <benkoalex521@gmail.com>
2020-06-10 12:24:43 -0700

    Set up endpoint to send solo duo data to client

--- server/server.js
+++ server/server.js
@@ -3,55 +3,72 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const Cookies = require('universal-cookie');
+const RIOT_API_KEY = require('./rito.js');
+
+const getRankData    = require('./sr-solo-duo/getRankData.js');
+const getProfileData = require('./sr-solo-duo/getProfileData.js');
+const getChampData   = require('./sr-solo-duo/getChampData.js');
+
+//console.log(typeof sendSoloProfileData)
 
 const app = express();
-const port = process.env.PORT || 4200 ;
+const port = process.env.PORT || 4242 ;
 
-app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 app.use(morgan('dev'));
 
-app.use(session({
-  key: 'user_sid',
-  secret: 'somerandonstuffs',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-      expires: 6000000
-  }
-}));
+//for some reason importing this function from another file converted it to an object
+//TODO: Figure out why and how to fix it
+//TODO: Refractor to be promies instead of this callback hell
+let sendSoloProfileData = (summoner,KEY,cb) =>{
+  let profileData = {};
+
+  getProfileData(summoner,KEY,(profile)=>{
+    profileData.id            = profile["id"];
+    profileData.name          = profile["name"];
+    profileData.summonerLevel = profile["summonerLevel"];
+    profileData.profileIconId = profile["profileIconId"];
+
+    getRankData(profileData.id,KEY,(rank)=>{
+      //order of the array returned can change from day to day
+      if(rank[0]["queueType"] === "RANKED_SOLO_5x5"){
+        profileData.tier = rank[0]["tier"];
+        profileData.rank = rank[0]["rank"];
+        profileData.wins = rank[0]["wins"];
+        profileData.hotStreak = rank[0]["hotStreak"];
+
+      } else {
+        profileData.tier = rank[1]["tier"];
+        profileData.rank = rank[1]["rank"];
+        profileData.wins = rank[1]["wins"];
+        profileData.hotStreak = rank[1]["hotStreak"];
+      }
+      getChampData(profileData.id,KEY,(champData)=>{
+        console.log(profileData)
+        profileData.champData = champData;
+        //ran into a scoping issue where i called the callbaxk outside of this and it send back an empty object
+        cb(profileData);
+      })
+    })
+  })
+
+};
 
 
-/*
-app.use(session({
-  secret: 'keyboard cat',
-  resave: true,
-  saveUninitialized: true
-}))
+app.use('/sr/:summoner',(req,res)=>{
+  let toGet = req.params.summoner
+  console.log(`Getting Profile Data For ${toGet}...`);
 
-app.get('/bop', function(req, res){
-  if(req.session.page_views){
-     req.session.page_views++;
-     res.send("You visited this page " + req.session.page_views + " times");
-  } else {
-     req.session.page_views = 1;
-     res.send("Welcome to this page for the first time!");
-  }
-});
-*/
+  sendSoloProfileData(toGet,RIOT_API_KEY,(results)=>{
 
-/*
-app.use('/cookie', (req,res)=>{
-  const cookies = new Cookies(req.headers.cookie);
-  let response = cookies.get('myCat');
-  console.log(cookies.get('myCat'));
-  res.send(response);
+    //res.header("Access-Control-Allow-Headers", "x-requested-with, x-requested-by");
+    res.status(200).send(results);
+
+  })
 })
-*/
+
+
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 app.listen(port, () => console.log(`app listening at http://localhost:${port}`));
