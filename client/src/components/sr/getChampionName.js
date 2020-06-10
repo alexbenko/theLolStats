f1b14570608e917034ce8fbdacb8da83f6746fb6
Alex Benko <benkoalex521@gmail.com>
2020-06-09 20:33:18 -0700

    Algorithm now returns proper name required to display the correct champion image

--- client/src/components/sr/getChampionName.js
+++ client/src/components/sr/getChampionName.js
@@ -1,21 +1,36 @@
 //converts the championid to a champion name
 import champInfo from './championId.js';
 //const champInfo = require('./championId.js');
+const capitalizeFirstLetter = (string) =>{
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 let getChampionName = (idPair) =>{
   let champName = champInfo[idPair];
 
-  if(champName.includes("'")){
+  if(champName === "Nunu & Willump"){
+    return "Nunu";
+  } else if(champName === "Rek\'Sai"){
+    return "RekSai";
+  } else if(champName === "LeBlanc"){
+    return "Leblanc"
+  } else if(champName === "Dr. Mundo"){
+    return "DrMundo";
+  } else if(champName === "Kog\'Maw"){
+    return "KogMaw";
+  }else if(champName === "Wukong"){
+    return "MonkeyKing";
+  }else if(champName.includes("'")){
     //champName.replace("'", "");
     let regex = /\'/;
-    let newChampName = champName.replace(regex,"");
-    console.log(newChampName);
-    return newChampName
+    let newChampName = champName.replace(regex,"").toLowerCase();
+
+    return capitalizeFirstLetter(newChampName);
   } else if(champName.includes(" ")) {
     let regex = / /;
     let newChampName = champName.replace(" ","");
-    console.log(newChampName);
-    return newChampName
+
+    return newChampName;
   } else {
     return champName;
   }
