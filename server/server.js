6ad7822b88cc0861f0a30f0977ebd88e9b363aea
Alex Benko <benkoalex521@gmail.com>
2020-06-11 10:13:27 -0700

    Used new promise based profile request function

--- server/server.js
+++ server/server.js
@@ -4,12 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 const RIOT_API_KEY = require('./rito.js');
-
-const getRankData    = require('./sr-solo-duo/getRankData.js');
-const getProfileData = require('./sr-solo-duo/getProfileData.js');
-const getChampData   = require('./sr-solo-duo/getChampData.js');
-
-//console.log(typeof sendSoloProfileData)
+const sendSoloProfileDataPromise = require('./sr-solo-duo-promises/sendProfileData.js');
 
 const app = express();
 const port = process.env.PORT || 4242 ;
@@ -19,64 +14,19 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 app.use(morgan('dev'));
 
-const checkIfUserHasRank = (rankInfo) =>{
-  if(rankInfo.length === 0 ) {
-    return (' Unranked');
-  } else {
-    return (rankInfo.tier + ' ' + rankInfo.rank);
-  }
-};
-
-const checkWins = (rankInfo) =>{
-  if(rankInfo.length === 0) {
-    return (' No Ranked Data Available For this Season')
-  } else {
-    return (rankInfo.wins)
-  }
-};
-//for some reason importing this function from another file converted it to an object
-//TODO: Figure out why and how to fix it
-//TODO: Refractor to be promies instead of this callback hell
-let sendSoloProfileData = (summoner,KEY,cb) =>{
-  let profileData = {};
-
-  getProfileData(summoner,KEY,(profile)=>{
-    profileData.id            = profile["id"];
-    profileData.name          = profile["name"];
-    profileData.summonerLevel = profile["summonerLevel"];
-    profileData.profileIconId = profile["profileIconId"];
-
-    getRankData(profileData.id,KEY,(rank)=>{
-      //order of the array returned can change from  day to day
-        let wins = checkWins(rank[0]);
-        let rankAndTier = checkIfUserHasRank(rank[0]);
-
-        profileData.rank = rankAndTier;
-        profileData.wins = wins;
-        profileData.hotStreak = rank[0]["hotStreak"];
-
-      getChampData(profileData.id,KEY,(champData)=>{
-
-        profileData.champData = champData;
-        //ran into a scoping issue where i called the callbaxk outside of this and it send back an empty object
-        cb(profileData);
-      })
-    })
-  })
-
-};
-
-
 app.use('/sr/:summoner',(req,res)=>{
   let toGet = req.params.summoner
   console.log(`Getting Profile Data For ${toGet}...`);
 
-  sendSoloProfileData(toGet,RIOT_API_KEY,(results)=>{
-
-    //res.header("Access-Control-Allow-Headers", "x-requested-with, x-requested-by");
+  sendSoloProfileDataPromise(toGet,RIOT_API_KEY)
+  .then((results)=>{
     res.status(200).send(results);
-
   })
+  .catch((err)=>{
+    console.log(err)
+    res.status(404).send("Error Finding Summoner ¯\_(ツ)_/¯...");
+  })
+
 })
 
 
