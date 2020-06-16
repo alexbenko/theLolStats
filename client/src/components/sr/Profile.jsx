2b6120246b05f3629238395fdace8df4313c6b98
Alex Benko <benkoalex521@gmail.com>
2020-06-16 16:17:24 -0700

    Imporved conditional rendering

--- client/src/components/sr/Profile.jsx
+++ client/src/components/sr/Profile.jsx
@@ -1,49 +1,50 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
-import { makeStyles } from '@material-ui/core/styles';
 import WhatshotIcon from '@material-ui/icons/Whatshot';
+import Grid from '@material-ui/core/Grid';
 
 const capitalizeFirstLetter = (string) =>{
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-//<img src={`https://rankedimages.s3-us-west-2.amazonaws.com/Emblem_${capitalizeFirstLetter((props.rank[0].tier).toLowerCase())}.png`}
-//commenting this image out until I have i redo the image hosting
-
-let Profile = ({profile}) => {
+const onHotstreak = (profile) =>{
   if(profile.hotStreak){
-    return(
-      <div className="prof">
-      <h3>{`Summoner: ${profile.name}`}</h3>
-
-      <Avatar src={`http://ddragon.leagueoflegends.com/cdn/10.9.1/img/profileicon/${profile.profileIconId}.png`} style={{width:"10em", height:"10em"}}/>
-
-      <div style={{display: "flex", flexDirection:"row",paddingBottom:"3%"}}>
+    return (
+      <div style={{display: "flex", flexDirection:"row"}}>
         <WhatshotIcon style={{ color: "red",fontSize: "50" }}/>
-        <h2 style={{paddingTop:"1%"}}>On A Win Streak</h2>
+        <h2 style={{paddingTop:"2.5%"}}>On A Win Streak</h2>
       </div>
-
-      <h3>{`Level: ${profile.summonerLevel}`}</h3>
-      <h3>Solo-Duo Rank:{profile.rank}</h3>
-      <h3>Total Wins This Season:{profile.wins}</h3>
-    </div>
     )
-  } else {
-    return(
-      <div className="prof">
-      <h3>{`Summoner: ${profile.name}`}</h3>
-
-        <Avatar src={`http://ddragon.leagueoflegends.com/cdn/10.9.1/img/profileicon/${profile.profileIconId}.png`} style={{width:"10em", height:"10em"}}/>
-      <h3>{`Level: ${profile.summonerLevel}`}</h3>
-
-      <h3>Solo-Duo Rank:{profile.rank}</h3>
-      <h3>Total Wins This Season:{profile.wins}</h3>
-    </div>
-    )
-
   }
+}
+
+let style = {
+  paddingLeft:"25%",
+  paddingBottom:"2%",
+  fontSize:"10vw"
+}
+
+let Profile = ({profile}) =>{
+  return (
+    <div className="prof" style={style}>
+    <h1>{`${profile.name}`}</h1>
+
+    <Avatar src={`http://ddragon.leagueoflegends.com/cdn/10.9.1/img/profileicon/${profile.profileIconId}.png`} style={{width:"10em", height:"10em"}}/>
+
+    <div style={{display: "flex", flexDirection:"row",paddingBottom:"3%"}}>
+      {onHotstreak(profile)}
+    </div>
+    <div className="profile-data" style={{fontSize:"2vw",paddingRight:"15%"}}>
+      <p>{`Level: ${profile.summonerLevel}`}</p>
+      <p>Solo-Duo Rank:{profile.rank}</p>
+      <p>Total Wins This Season:{profile.wins}</p>
+      <p>Season Win Rate: {profile.totalWr} %</p>
+    </div>
+  </div>
+  )
 
 };
+
 
 
 export default Profile
