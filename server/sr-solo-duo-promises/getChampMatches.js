f0d531df6afca65dc25a5de8ba65fa9ad3640564
Alex Benko <benkoalex521@gmail.com>
2020-06-18 16:19:10 -0700

    Function that will retrieve match history for summoner

---
+++ server/sr-solo-duo-promises/getChampMatches.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+const getChampMatches = (actId,KEY) =>{
+
+  return new Promise((resolve,reject)=>{
+    let target = `https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${actId}&queue=420&season=13&api_key=${KEY}`;
+    axios.get(target)
+    .then((data)=>{
+      resolve(data.data)
+    })
+    .catch((err)=>{
+      console.log(err)
+      reject(new Error(err))
+    })
+
+  })
+
+}
+
+
+
+
+module.exports = getChampMatches;
